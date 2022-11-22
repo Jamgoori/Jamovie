0b36a1ce3b796a2a7dc0d8f089079e62dc2f7f82
Jamgoori <lks2354@naver.com>
2022-11-22 22:16:06 +0900

    Promiseall

--- src/redux/actions/movieActions.js
+++ src/redux/actions/movieActions.js
@@ -3,8 +3,12 @@ import api from "../api"
 const API_KEY=process.env.REACT_APP_API_KEY
 function getMovies(){
     return async (dispatch)=>{
-        const popularMovieApi = await api.get(`movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+        const popularMovieApi = api.get(`movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+        const topRateApi = api.get(`movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`)
+        const upComingApi = api.get(`movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`)
 
+       let data = await Promise.all([popularMovieApi,topRateApi,upComingApi])
+       console.log("Promise로 불러오는 데이터",data)
 }}
 
 export const movieActions = {
