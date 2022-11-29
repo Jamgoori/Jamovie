3cc5f8b1ac458af21baca40181676eecd815afa2
Jamgoori <lks2354@naver.com>
2022-11-29 23:34:56 +0900

    Redux-Toolkit 리팩토리중

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,26 +7,18 @@ import ClipLoader from "react-spinners/ClipLoader";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const { popularMovies, topRatedMovies, upcomingMovies, loading } = useSelector(
-    (state) => state.movie
-  );
+  const { popularMovies, topRatedMovies, upcomingMovies, loading } =
+    useSelector((state) => state.movie);
   useEffect(() => {
     dispatch(movieActions.getMovies());
   }, []);
 
-  //loading이 true라면 스피너를 보여준다. false면 데이터를 보여준다.
-  //true : 데이터 도착전  false: 데이터 도착후, 에러가 났을 때
-  if(loading){
-    return       <ClipLoader
-    color="red"
-    loading={loading}
-    size={150}
-  />
+  if (loading) {
+    return <ClipLoader color="red" loading={loading} size={150} />;
   }
   return (
     <div>
-
-    <Banner movie={popularMovies.results[0]} />
+      <Banner movie={popularMovies.results[0]} />
 
       <h1>Popular Movie</h1>
       <MovieSlide movie={popularMovies} />
