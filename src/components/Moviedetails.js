ec3f8dbc86a4d120383a28d2e764de6cc5431ec1
Jamgoori <lks2354@naver.com>
2022-11-30 14:13:51 +0900

    주석제거

---
+++ src/components/Moviedetails.js
@@ -0,0 +1,18 @@
+import React,{useEffect} from 'react'
+import Banner from './Banner'
+
+const Moviedetails = () => {
+    const getMovieDetail=()=>{
+        
+    }
+    useEffect(()=>{
+        getMovieDetail()
+    })
+  return (
+    <>
+    <Banner/>
+    </>
+  )
+}
+
+export default Moviedetails
