9fb7837286783070e4ed3a2dd16740eac33d6374
Jamgoori <lks2354@naver.com>
2022-12-15 12:27:59 +0900

    리듀서 수정

--- src/redux/reducers/movieFilterReducer.js
+++ src/redux/reducers/movieFilterReducer.js
@@ -22,21 +22,42 @@ const filterSlice = createSlice({
         getMovieFilterRequest(state,action){
             state.loading = action.true;
         },
-        getMovieFilterSuccess(state,action){
+        storeMovieGenresSuccess(state,action){
             state.withGenres = action.payload.withGenres;
+        },
+        storeMovieSortSuccess(state,action){
             state.sortBy = action.payload.sortBy;
+        },
+        getFilteredMoviesRequest(state){
+            
+        },
+        getFilteredMoviesSuccess(state,action){
             state.genreListData = action.payload.genreList.data;
+            state.loading = action.false;
+        },
+        getMoreMoviesSuccess(state,action){
             state.moreMoviesData = action.payload.moreMoviesData;
-            state.moreMoviesDataLoading = action.payload.moreMoviesDataLoading;
+            state.moreMoviesDataLoading = action.false;
+            state.loading = action.false;
+        },
+        searchKeywordStoreSuccess(state,action){
             state.keyword = action.payload.keyword;
+        },
+        releaseDateFilterStoreSuccess(state,action){
             state.releaseDateGte = action.payload.date_gte;
             state.releaseDateLte = action.payload.date_lte;
+        },
+        scoreFilterStoreSuccess(state,action){
             state.voteAverageGte = action.payload.vote_gte;
             state.voteAverageLte = action.payload.vote_lte;
+        },
+        includeMovieVideoToggleSuccess(state,action){
             state.includeVideo = action.payload.includeVideo;
+
+        },
+        resetMoviesSearchSuccess(state,action){
             state.keyword = {};
             state.loading = action.false;
-
         },
         getMovieFilterFail(state,action){
             state.loading = action.false;
