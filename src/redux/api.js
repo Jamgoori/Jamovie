0e215c62ccbcdfbbe52af3475172d8de1499c5b8
Jamgoori <lks2354@naver.com>
2022-12-22 14:37:17 +0900

    콘솔제거

--- src/redux/api.js
+++ src/redux/api.js
@@ -7,23 +7,18 @@ const api = axios.create({
 
 api.interceptors.request.use(
   function (config) {
-    console.log("request start", config);
     return config;
   },
   function (error) {
-    console.log("request error", error);
     return Promise.reject(error);
   }
 );
 
 api.interceptors.response.use(
   function (response) {
-
-    console.log("get response", response);
     return response;
   },
   function (error) {
-    console.log("response error", error);
     return Promise.reject(error);
   }
 );
